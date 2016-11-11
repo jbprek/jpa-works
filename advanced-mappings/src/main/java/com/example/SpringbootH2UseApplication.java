e2b91f8fb875e897b224610c5db8e877263be545
prekezes <iP4$l1mak!s>
2016-11-11 17:00:18 +0200

    spring boot h2 example

---
+++ advanced-mappings/src/main/java/com/example/SpringbootH2UseApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootH2UseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootH2UseApplication.class, args);
+	}
+}
---
+++ spring-boot-h2-use/src/main/java/com/example/SpringBootH2UseApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootH2UseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2UseApplication.class, args);
+	}
+}
